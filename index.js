548adb303d5524a60150b9d35f3a6dc2550fb7f2
burningTyger <b6tyger@gmail.com>
2019-03-14 20:52:50 +0100

    node backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const send = require('@polka/send');
+const db = require('better-sqlite3')('farhang.db');
+db.loadExtension('./libicu.so');
+
+const Trouter = require('trouter');
+const router = new Trouter();
+router.get('/a/:term', (req, res) => {
+  return db.prepare('SELECT * FROM lemmas WHERE lemma LIKE ? ORDER BY lemma ').all(req.params.term + '%');
+});
+router.get('/g/:id', (req, res) => {
+  return db.prepare('SELECT * FROM translations WHERE lemma_id = ?').all(req.params.id);
+});
+
+function next(err) {
+	if (err) throw err;
+}
+
+async function loop (arr, req, res) {
+	let fn, out;
+	for (fn of arr) {
+		out = await fn(req, res, next) || out;
+	}
+	return out;
+}
+
+module.exports = async function (req, res) {
+	let obj = router.find(req.method, req.url);
+	if (!obj) return send(res, 404, 'Not found');
+
+	try {
+    req.params = obj.params;
+    return await loop(obj.handlers, req, res);
+	} catch (err) {
+		send(res, err.statusCode || 400, err.message || err);
+	}
+}
